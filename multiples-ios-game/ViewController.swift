a10506420279c5e589f823dfc1769b1e7ffffaea
Ishan Shams <ishanshams@Ishans-MBP.home>
2016-02-05 20:36:27 -0500

    final ios product complete

--- multiples-ios-game/ViewController.swift
+++ multiples-ios-game/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //variables
+    var chosenNum : Int = 0
+    var total : Int = 0
+    
+    //outlets
+    @IBOutlet weak var logo : UIImageView!
+    @IBOutlet weak var playBtn : UIButton!
+    @IBOutlet weak var textFld : UITextField!
+    
+    @IBOutlet weak var textLbl : UILabel!
+    @IBOutlet weak var addBtn : UIButton!
+    
+    @IBAction func playButton(sender : UIButton!) {
+        if textFld.text != nil && textFld.text != "" {
+            logo.hidden = true
+            textFld.hidden = true
+            playBtn.hidden = true
+            
+            addBtn.hidden = false
+            textLbl.hidden = false
+            
+            chosenNum = Int(textFld.text!)!
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func addButton(sender : UIButton!) {
+        
+        
+        textLbl.text = "\(total) + \(chosenNum) = \(total+chosenNum)"
+        total += chosenNum
+        
+        if total > 100 {
+            restartGame()
+        }
     }
-
-
+    
+    func restartGame() {
+        chosenNum = 0
+        total = 0
+        textLbl.text = "Press ADD to add!"
+        logo.hidden = false
+        textFld.hidden = false
+        playBtn.hidden = false
+        
+        addBtn.hidden = true
+        textLbl.hidden = true
+        
+    }
+    
+    
 }
 
